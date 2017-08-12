f8596a7bf9729fe2613d9ac0d0fab93c15d88346
TRomesh <tharakaromesh@gmail.com>
2017-08-12 20:39:45 +0530

    Add wunderground API key

--- src/providers/weather/weather.ts
+++ src/providers/weather/weather.ts
@@ -10,9 +10,18 @@ import 'rxjs/add/operator/map';
 */
 @Injectable()
 export class WeatherProvider {
+  apiKey='0fc2c6f2e16c0082';
+  url;
+
 
   constructor(public http: Http) {
     console.log('Hello WeatherProvider Provider');
+    this.url='http://api.wunderground.com/api/'+this.apiKey+'/conditions/q';
   }
+
+  getWeather(city,state){
+    return this.http.get(this.url+'/'+state+'/'+city+'.json')
+           .map(res => res.json());
+  };
 
 }
